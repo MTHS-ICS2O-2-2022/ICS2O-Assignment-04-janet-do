8b20f3efcf5cbbd968e79581982d847b0882e22e
janet-do <124389477+janet-do@users.noreply.github.com>
2023-04-30 19:20:22 +0000

    sync 5

--- js/script.js
+++ js/script.js
@@ -2,16 +2,25 @@
 //
 // Created by: Janet Do
 // Created on: Sep 2020
-// This file generates the area of a triangle
+// This file generates the cost of groceries depending on quantity
+
 "use strict"
-function calculate() {
+
+function myButtonClicked() {
+  const deductions = 0.13
   // input
-  const base = parseInt(document.getElementById("base").value)
-  const height = parseInt(document.getElementById("height").value)
+  const item = parseInt(document.getElementById("item").value)
+  const quantity = parseInt(document.getElementById("quantity").value)
 
   // process
-  const area = base * height / 2
+  const subTotal = item * quantity
+  const tax = item * quantity * deductions
+  const finalTotal = subTotal + tax
 
   // output
-  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+  document.getElementById("subTotal").innerHTML =
+    "Sub total is:" + subTotal.toFixed(2)
+  document.getElementById("finalTotal").innerHTML =
+    "Your final total is:" + finalTotal.toFixed(2)
+  document.getElementById("tax").innerHTML = "Tax: $" + tax.toFixed(2)
 }
